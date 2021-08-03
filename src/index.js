7a4af970d4a8b9a7773889e52fb61388e3ad32b2
james-roberts-tfs <james.roberts2@thermofisher.com>
2021-08-03 15:18:37 -0700

    add magic number for eslint to find

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
  
 server.listen(port, host, () => {
-   console.log('Web server running at http://%s:%s',host,port );
+   console.log('Web server running at http://%s:%s',host,2999); // port );
 });
