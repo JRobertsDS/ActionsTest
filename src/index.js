c110228c88a56069c3dce645158a402d39468b78
james-roberts-tfs <james.roberts2@thermofisher.com>
2021-08-03 15:37:47 -0700

    force new reviewdog review

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello World!');
 });
  
-console.log('Web server running at http://%s:%s',host,2989); // port ); add second magic number
+console.log('Web server running at http://%s:%s',host,2939); // port ); add second magic number
 
 server.listen(port, host, () => {
    console.log('Web server running at http://%s:%s',host,2999); // port );
